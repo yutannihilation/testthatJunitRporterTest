c0942f7d194b7428ad9e67dcbac07ac8224f0c3e
Hiroaki Yutani <yutani.ini@gmail.com>
2017-10-18 17:33:20 +0900

    add reporter

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,10 @@
 library(testthat)
 library(testthatJunitRporterTest)
 
-set_reporter(JunitReporter$new())
-
-test_check("testthatJunitRporterTest")
+test_check("testthatJunitRporterTest",
+           reporter = MultiReporter$new(
+             list(
+               SummaryReporter$new(),
+               JunitReporter$new()
+             )
+           ))
