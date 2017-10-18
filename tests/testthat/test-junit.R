f2d7ff17bb8083a76961b36cadacca4a47cf3aaa
Hiroaki Yutani <yutani.ini@gmail.com>
2017-10-18 17:56:15 +0900

    add more tests

--- tests/testthat/test-junit.R
+++ tests/testthat/test-junit.R
@@ -1,6 +1,13 @@
 context("junit")
 
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  expect_equal(1 * 2, 2)
+  expect_equal(2 * 2, 3)
+  expect_equal(3 * 2, 6)
+})
+
+test_that("summation works", {
+  expect_equal(1 + 1, 2)
   expect_equal(2 + 2, 5)
+  expect_equal(3 + 3, 6)
 })
