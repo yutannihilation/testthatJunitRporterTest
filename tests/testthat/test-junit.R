134e2b4bae36683b574b7186144c42b942f28f01
Hiroaki Yutani <yutani.ini@gmail.com>
2017-10-18 18:00:23 +0900

    add one more failure

--- tests/testthat/test-junit.R
+++ tests/testthat/test-junit.R
@@ -4,10 +4,12 @@ test_that("multiplication works", {
   expect_equal(1 * 2, 2)
   expect_equal(2 * 2, 3)
   expect_equal(3 * 2, 6)
+  expect_equal(4 * 2, 6)
 })
 
 test_that("summation works", {
   expect_equal(1 + 1, 2)
   expect_equal(2 + 2, 5)
   expect_equal(3 + 3, 6)
+  expect_equal(4 + 4, 8)
 })
